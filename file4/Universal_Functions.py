0ded263eb8193e357430e498fa641d0f030d3ba2
Qata-Mo7ammad-Isma3el <imoh40398@gmail.com>
2025-09-08 18:51:18 +0300

    test3

---
+++ file4/Universal_Functions.py
@@ -0,0 +1,11 @@
+#https://numpy.org/doc/stable/reference/ufuncs.html
+
+#https://jakevdp.github.io/PythonDataScienceHandbook/02.03-computation-on-arrays-ufuncs.html
+"""
+
+the main idea behind numpy library is to make things faster by manipulate an array and perform a 
+lot of mathematical operations and much more with less time and space 
+i attached a link to numpy website that have full ufuncs documentations 
+how it works and what is the idea behind each function 
+
+"""
